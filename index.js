a91f39aefe82a2d2e2df2eff1a61a6924772eb42
Ayush Singh <ayushsingh638684@gmail.com>
2026-04-18 07:44:44 +0530

    Add initial CI pipeline configuration and basic project files

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello world");
+console.log("I'm learning CI/CD pipelines with GitHub Actions");
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Starting test ....");
+setTimeout(() => {
+    console.log("waiting 3 seconds ....");  
+}, 3000);
+console.log("test completed");
+
